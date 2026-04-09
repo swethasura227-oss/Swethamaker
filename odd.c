60aac5695dbbfe9acdaa55a1a0c62b6f82e1fbb6
swethasura227-oss <swethasura227@gmail.com>
2026-04-09 16:10:20 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("enter the number:\n");
+    scanf("%d",&num);
+    if (num%2==0)
+    {
+        printf("it is not odd number");
+    }
+    else
+    {
+      printf("it is odd number");
+        }
+}
